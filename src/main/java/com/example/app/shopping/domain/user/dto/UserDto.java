5529f86886c11a0ec389cdb8b88f388c6de971d9
pakms980319 <pakms980319@gmail.com>
2024-05-21 17:49:36 +0900

    DB 자료추가2

---
+++ src/main/java/com/example/app/shopping/domain/user/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.app.shopping.domain.user.dto;
+
+
+public class UserDto {
+    private String id;
+    private String password;
+    private String pwChkHint;
+    private String pwChkAns;
+    private String zipcode;
+    private String streetAdr;
+    private String detailAdr;
+    private String phone;
+    private String email;
+    private String role;
+    private String provider;
+    private String providerId;
+}

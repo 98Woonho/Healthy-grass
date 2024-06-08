38e818ea5c801f22f19a7a1f52e802f8061fcd39
98Woonho <9woonho8@gmail.com>
2024-06-08 10:34:42 +0900

    관리자 제품 등록 구현중

--- src/main/resources/static/js/admin/addProduct.js
+++ src/main/resources/static/js/admin/addProduct.js
@@ -1,5 +1,6 @@
 const addProductForm = document.getElementById('addProductForm');
-const uploadBoxes = document.querySelectorAll('.upload-box');
+const mainUploadBox = document.getElementById('mainUploadBox');
+const subUploadBox = document.getElementById('subUploadBox');
 
 // 카테고리 선택 function
 const changeCategorySelect = (select) => {
@@ -16,27 +17,75 @@ const changeCategorySelect = (select) => {
     }
 }
 
+let mainImage;
 
+// 메인 이미지 등록 dragover event
+mainUploadBox.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    mainUploadBox.style.opacity = '0.5';
+});
 
-// 이미지 uploadBox에 drop event
-uploadBoxes.forEach(uploadBox => {
-    uploadBox.addEventListener('dragover', (e) => {
-        e.preventDefault();
-        uploadBox.style.opacity = '0.5';
-    });
+// 메인 이미지 등록 drop event
+mainUploadBox.addEventListener('drop', (e) => {
+    e.preventDefault();
 
-    uploadBox.addEventListener('drop', (e) => {
-        e.preventDefault();
+    const mainImages = e.dataTransfer.files;
+    mainImage = e.dataTransfer.files[0];
 
-        const imgFiles = Array.from(e.dataTransfer.files).filter(f => f.type.startsWith('image/'));
+    // 여러 개 drop 시 경고문 출력
+    if (mainImages.length > 1) {
+        alert('메인 이미지는 한 개만 등록 가능합니다.');
+        return;
+    }
 
-        if (imgFiles.length === 0) {
-            alert("이미지 파일만 가능합니다.");
-            return;
-        }
-    })
+    // 이미지가 아닌 파일 drop 시 경고문 출력
+    if (!mainImages[0].type.startsWith('image/')) {
+        alert('이미지 파일만 가능합니다.');
+        return;
+    }
+
+    // 이미지 파일 용량 제한
+    if (mainImage.size > (1024 * 1024 * 5)) {
+        alert('파일 하나당 최대 사이즈는 5MB이하여야 합니다.');
+    }
+
+    const reader = new FileReader(); // FileReader
+    const mainPreview = document.getElementById('mainPreview');
+
+    // 이미지가 등록되어있는 상황에서 더 등록할 시, 경고문 출력
+    if (mainPreview.querySelectorAll('.item').length === 1) {
+        alert('대표 이미지는 한 개만 등록 가능합니다.');
+        return;
+    }
+
+    reader.readAsDataURL(mainImage); // reader에 image 정보 넣기.
+    reader.onload = function (e) {
+        const src = e.target.result;
+
+        const item = new DOMParser().parseFromString(`
+                <li class="item">
+                    <input hidden type="text" class="file-name" name="mainFileName" th:value="${mainImage.name}">
+                    <img src="${src}" alt="">
+                    <button type="button" id="deleteBtn">삭제</button>
+                </li>
+            `, 'text/html').querySelector('.item');
+        const deleteBtn = item.querySelector('#deleteBtn');
+
+        // 메인 이미지 프리뷰에 이미지 item 추가
+        mainPreview.append(item);
+
+        // 삭제 버튼 클릭 event
+        deleteBtn.addEventListener('click', () => {
+            item.remove();
+        })
+    }
 })
 
 addProductForm.onsubmit = (e) => {
     e.preventDefault();
+
+    console.log(mainImage);
+
+    const formData = new FormData(this);
+    formData.append('mainImage', mainImage); // 추가를 원함
 }

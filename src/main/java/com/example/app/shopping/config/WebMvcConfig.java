772d393ba3b4782abb8e0b2e9cd457066acc2086
pakms980319 <pakms980319@gmail.com>
2024-06-11 15:33:23 +0900

    할인된 가격에서 일의 자리 삭제

--- src/main/java/com/example/app/shopping/config/WebMvcConfig.java
+++ src/main/java/com/example/app/shopping/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {

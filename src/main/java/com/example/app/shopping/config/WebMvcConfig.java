d89f68682dd410165057386a171bb430bcf15c9c
pakms980319 <pakms980319@gmail.com>
2024-06-11 14:50:39 +0900

    EnableWebMvc 어노테이션으로 인한 버그 수정, 이후 발생하는 버그있다면 제보바람

--- src/main/java/com/example/app/shopping/config/WebMvcConfig.java
+++ src/main/java/com/example/app/shopping/config/WebMvcConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -15,6 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/"); //.setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/json/**").addResourceLocations("classpath:/static/json/"); //.setCachePeriod(60*60*24*365);
         registry.addResourceHandler("/font/**").addResourceLocations("classpath:/static/font/");//.setCachePeriod(60*60*24*365);
+
         // http://localhost:8080/imageboard/[이미지경로] 입력하면 이미지가 나옴. 이게 없으면 이미지 접근 불가능
         registry.addResourceHandler("/shopping/**").addResourceLocations("file:/shopping/");//.setCachePeriod(60*60*24*365);
     }

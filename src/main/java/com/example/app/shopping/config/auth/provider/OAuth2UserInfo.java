c521b04419d20fe36da93d0152a602ae91fc09b0
jungwoogyun <jwg8910@naver.com>
2024-06-19 12:28:26 +0900

    .

---
+++ src/main/java/com/example/app/shopping/config/auth/provider/OAuth2UserInfo.java
@@ -0,0 +1,12 @@
+package com.example.app.shopping.config.auth.provider;
+
+import java.util.Map;
+
+public interface OAuth2UserInfo {
+    String getName();
+    String getEmail();
+    String getProvider();
+    String getProviderId();
+
+    Map<String,Object> getAttributes();
+}
--- src/main/java/com/example/app/shopping/controller/HomeController.java
+++ src/main/java/com/example/app/shopping/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.app.shopping.controller;
 
+import com.example.app.shopping.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
-    public String home() {
+    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println("GET / ..." + principalDetails);
         return "index";
     }
+
 }

e52eb777c1a59f8bb7f444b29bbc9a9369370e42
pakms980319 <pakms980319@gmail.com>
2024-06-26 14:22:20 +0900

    db

--- src/main/java/com/example/app/shopping/config/DataSourceConfig.java
+++ src/main/java/com/example/app/shopping/config/DataSourceConfig.java
@@ -1,19 +1,26 @@
 package com.example.app.shopping.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
+    @Value("${spring.database.mysql.username}")
+    private String username;
+    @Value("${spring.database.mysql.password}")
+    private String password;
+
     @Bean
     public HikariDataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/shopping");
-        dataSource.setUsername("root");
-        dataSource.setPassword("1234");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
